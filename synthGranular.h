7a2cdaa250e6fb20a60633afd636f567e16dcbe4
Alexandre Piel <alexandre.piel@zooplus.com>
2023-08-14 22:54:54 +0200

    grain

--- synthGranular.h
+++ synthGranular.h
@@ -7,20 +7,69 @@
 #include "def.h"
 #include "effectGranular.h"
 
-class SynthGranular {
+// class SynthGranular {
+// protected:
+//     AudioBuffer<GRANULER_BUFFER_SECONDS> buffer;
+
+// public:
+//     EffectGranular granular;
+//     SF_INFO sfinfo;
+//     SNDFILE* file = NULL;
+
+//     SynthGranular()
+//     : granular(&buffer)
+//     {
+//         memset(&sfinfo, 0, sizeof(sfinfo));
+//         // open("samples/0_0_axxe13.wav");
+//     }
+
+//     ~SynthGranular()
+//     {
+//         close();
+//     }
+
+//     SynthGranular& close()
+//     {
+//         if (file) {
+//             sf_close(file);
+//         }
+//         return *this;
+//     }
+
+//     SynthGranular& open(const char* filename, int seek = SEEK_END)
+//     {
+//         close();
+
+//         if (!(file = sf_open(filename, SFM_READ, &sfinfo))) {
+//             APP_INFO("Error: could not open file %s\n", filename);
+//             return *this;
+//         }
+//         APP_INFO("Audio file %s sampleCount %ld sampleRate %d\n", filename, (long)sfinfo.frames, sfinfo.samplerate);
+
+//         sf_read_float(file, buffer.samples, buffer.size);
+
+//         return *this;
+//     }
+
+//     float sample()
+//     {
+//         return granular.sample();
+//     }
+// };
+
+
+class SynthGranular: public EffectGranular {
 protected:
     AudioBuffer<GRANULER_BUFFER_SECONDS> buffer;
 
 public:
-    EffectGranular granular;
     SF_INFO sfinfo;
     SNDFILE* file = NULL;
 
     SynthGranular()
-    : granular(&buffer)
+    : EffectGranular(&buffer)
     {
         memset(&sfinfo, 0, sizeof(sfinfo));
-        // open("samples/0_0_axxe13.wav");
     }
 
     ~SynthGranular()
@@ -49,11 +98,6 @@ public:
         sf_read_float(file, buffer.samples, buffer.size);
 
         return *this;
-    }
-
-    float sample()
-    {
-        return granular.sample();
     }
 };
 
